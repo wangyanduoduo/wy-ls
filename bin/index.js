1e9f9b39ba85c53a918752dafcdf06f0bc08c3a5
wangyanduoduo <140472168+wangyanduoduo@users.noreply.github.com>
2023-12-29 14:17:36 +0800

    init

---
+++ bin/index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+/*
+ * @Author: wy
+ * @Date: 2023-12-29 14:06:31
+ * @LastEditors: wy
+ * @LastEditTime: 2023-12-29 14:07:22
+ * @FilePath: /笔记/web-cli/wy-ls/bin/index.js
+ * @Description:
+ */
+
+console.log('bin-index');
